198847d168cc02b167b2841060088985f35cd75e
Sanchit Thakur <jarvus92001@gmail.com>
2025-02-10 00:07:00 +0530

    changes in useGetUsrProfile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -65,7 +65,9 @@ const Profile = () => {
               <div className='flex flex-col gap-1'>
                 <span className='font-semibold'>{userProfile?.bio || 'bio here...'}</span>
                 <Badge className='w-fit' variant='secondary'><AtSign /> <span className='pl-1'>{userProfile?.username}</span> </Badge>
-                <span>🤯Turing code into fun</span>
+                <span>🤯 Hello World</span>
+                <span>🤯 Namaste Duniya</span>
+                
               </div>
             </div>
           </section>
--- src/hooks/useGetUserProfile.jsx
+++ src/hooks/useGetUserProfile.jsx
@@ -8,7 +8,7 @@ const useGetUserProfile = (userId) => {
     useEffect(()=>{
         const fetchdUserProfile = async () => {
             try {
-                const response = await axios.get(`http://localhost:8000/api/v1/user/${userId}/profile`);
+                const response = await axios.get(`http://localhost:8000/api/v1/user/${userId}/profile`, {withCredentials: true});
                 if(response.data.success){
                     dispatch(setUserProfile(response.data.user)); 
                 }
